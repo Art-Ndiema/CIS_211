a14edd9e0310fefbbd95ecf1041f9abad7fe7b15
Art Ndiema <106247754+Art-Ndiema@users.noreply.github.com>
2023-10-20 14:39:19 +0300

    Create login.php

---
+++ Data Manupilation/login.php
@@ -0,0 +1,14 @@
+<?php 
+//contains code showing how our login system will work session_start(); 
+include "dbconnect.php" ; 
+$uname = $_REQUEST['uname']; 
+$pwd = $_REQUEST['pwd']; 
+//confirming our variables are not empty 
+if(empty($uname)){ 
+header("Location : index.php?error = User Name is required!") ; 
+exit(); 
+} 
+else if(empty($pwd)){ 
+header("Location : index.php?error = Fill in your password. ") ; 
+exit(); 
+} 
